e48e6cd74f5ef1ac76d511e0282eacb9a265a84e
BWI <bwi@calculon.(none)>
2014-06-18 11:37:19 -0500

    fixed change_map consistancy bug

--- scripts/main.py
+++ scripts/main.py
@@ -17,6 +17,7 @@ map1 = None
 map2 = None
 map3 = None
 change_map = None
+flag = None
 # Subscribe to
     # TOPIC : 3 maps
     # SERVICE : Change map
@@ -51,8 +52,10 @@ initalPositionFloor3.pose.pose.position.y = 7.262574
 initalPositionFloor3.pose.pose.position.z = 0
 initalPositionFloor3.pose.pose.orientation.x = 0
 initalPositionFloor3.pose.pose.orientation.y = 0
-initalPositionFloor3.pose.pose.orientation.z = 0.704652683065
-initalPositionFloor3.pose.pose.orientation.w = 0.709552391476
+#initalPositionFloor3.pose.pose.orientation.z = 0.704652683065
+#initalPositionFloor3.pose.pose.orientation.w = 0.709552391476
+initalPositionFloor3.pose.pose.orientation.z = 0.6154
+initalPositionFloor3.pose.pose.orientation.w = 0.78819
 initalPositionFloor3.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
 
 
@@ -69,7 +72,8 @@ def map_mux():
     topic = rospy.resolve_name("map_metadata")
     pub_metadata = rospy.Publisher("map_metadata", MapMetaData)
     initialPosePub = rospy.Publisher("initialpose",  PoseWithCovarianceStamped)
-
+    global change_map
+    global flag
     # Service Servers
     s = rospy.Service('change_map', ChangeMap, changeMapfunc)
     s = rospy.Service('static_map', GetMap, staticMapfunc)
@@ -85,57 +89,59 @@ def map_mux():
     #rospy.wait_for_service("floor_switch")
 
 
-    r = rospy.Rate(10)
+    r = rospy.Rate(.1)
     old_change_map = 0
 
     while not rospy.is_shutdown():
         if( map1 == None  and map2 == None and map3 == None):
             print "you need to provide 3 maps on topics map1 map2 map3 from a launch file."
-
-
-        if (change_map != old_change_map):
+        #if (change_map != old_change_map):
+        if (1):
             if (change_map == 1 and map1 != None):
                 rospy.loginfo("changing to 1")
                 pub.publish(map1)
                 pub_metadata.publish(map1.info)
-            if (change_map == 2 and map2 != None):
+            if (change_map == 2 and map2 != None  and flag ==1 ):
                 rospy.loginfo("changing to 2")
                 pub.publish(map2)
                 pub_metadata.publish(map2.info)
                 #print str(type(initalPositionFloor2))
-                initialPosePub.publish(initalPositionFloor2)
                 try:
+                    initialPosePub.publish(initalPositionFloor2)
                     rosparam.set_param("/segbot_logical_navigator/door_file",
-                            rp.get_path("bwi_coffee") + "/config/multi_floor/atrium_doors.yaml")
+                            rp.get_path("bwi_kr") + "/config/multi_map/atrium_doors.yaml")
                     rosparam.set_param("/segbot_logical_navigator/location_file",
-                            rp.get_path("bwi_coffee") + "/config/multi_floor/atrium_locations.yaml")
+                            rp.get_path("bwi_kr") + "/config/multi_map/atrium_locations.yaml")
                     rosparam.set_param("/segbot_logical_navigator/object_file",
-                            rp.get_path("bwi_coffee") + "/config/multi_floor/atrium_objects.yaml")
+                            rp.get_path("bwi_kr") + "/config/multi_map/atrium_objects.yaml")
                     rosparam.set_param("/segbot_logical_navigator/map_file",
                             rp.get_path("map_mux") + "/maps/atrium_with_elevators.yaml")
 
                     resp_floor_switch = service_floor_switch(change_map)
                 except rospy.ServiceException:
                     print "floor switch service call failed"
-
-            if (change_map == 3 and map3 != None):
+                print "done with floor 2"
+                flag = 0
+            if (change_map == 3 and map3 != None and flag ==1 ):
                 rospy.loginfo("changing to 3")
                 pub.publish(map3)
                 pub_metadata.publish(map3.info)
-                initialPosePub.publish(initalPositionFloor3)
                 try:
+                    initialPosePub.publish(initalPositionFloor3)
                     rosparam.set_param("/segbot_logical_navigator/door_file",
-                            rp.get_path("bwi_coffee") + "/config/multi_floor/3ne_doors.yaml")
+                            rp.get_path("bwi_kr") + "/config/multi_map/3ne_doors.yaml")
                     rosparam.set_param("/segbot_logical_navigator/location_file",
-                            rp.get_path("bwi_coffee") + "/config/multi_floor/3ne_locations.yaml")
+                            rp.get_path("bwi_kr") + "/config/multi_map/3ne_locations.yaml")
                     rosparam.set_param("/segbot_logical_navigator/object_file",
-                            rp.get_path("bwi_coffee") + "/config/multi_floor/3ne_objects.yaml")
+                            rp.get_path("bwi_kr") + "/config/multi_map/3ne_objects.yaml")
                     rosparam.set_param("/segbot_logical_navigator/map_file",
                             rp.get_path("map_mux") + "/maps/map_whole2_with_elevators.yaml")
 
                     resp_floor_switch = service_floor_switch(change_map)
                 except rospy.ServiceException:
                     print "floor switch service call failed"
+                print "done with floor 3"
+                flag = 0
         old_change_map = change_map
         #rospy.sleep(1)
         #rospy.spin once()
@@ -154,8 +160,13 @@ def addMap3(data):
     global map3
     map3 = data
 def changeMapfunc( data):
+    print "the service made it here"
     global change_map
+    global flag
+    flag = 1
     change_map = data.data
+    while(1):
+        if (flag == 0): break
     return 1
 def staticMapfunc( data):
     global change_map
